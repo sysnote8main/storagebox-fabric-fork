b47635e205fcdf9547ed5c351dcbf82be36d0b46
sysnote8main <sysnote8main@gmail.com>
2024-03-08 22:53:47 +0900

    rename: mixin variables

--- src/main/java/ml/pkom/storagebox/mixin/RenderStorageBoxMixin.java
+++ src/main/java/ml/pkom/storagebox/mixin/RenderStorageBoxMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,11 +34,12 @@ public abstract class RenderStorageBoxMixin {
         }
     }
 
-    private static final ThreadLocal<ItemStack> RENDER_ITEM_OVERRIDING_FOR = new ThreadLocal<>();
+    @Unique
+    private static final ThreadLocal<ItemStack> storagebox$RENDER_ITEM_OVERRIDING_FOR = new ThreadLocal<>();
 
     @Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At("HEAD"), cancellable = true)
     protected void renderItem(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
-        if (RENDER_ITEM_OVERRIDING_FOR.get() == stack) return;
+        if (storagebox$RENDER_ITEM_OVERRIDING_FOR.get() == stack) return;
         if (!(stack.getItem() instanceof StorageBoxItem)) return;
         ClientWorld world = MinecraftClient.getInstance().world;
 
@@ -47,11 +49,11 @@ public abstract class RenderStorageBoxMixin {
         renderStack.setCount(1);
         BakedModel realModel = MinecraftClient.getInstance().getItemRenderer().getModels()
                 .getModel(renderStack);
-        RENDER_ITEM_OVERRIDING_FOR.set(stack);
+        storagebox$RENDER_ITEM_OVERRIDING_FOR.set(stack);
         try {
             this.renderItem(renderStack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, realModel);
         } finally {
-            RENDER_ITEM_OVERRIDING_FOR.remove();
+            storagebox$RENDER_ITEM_OVERRIDING_FOR.remove();
         }
         ci.cancel();
     }
